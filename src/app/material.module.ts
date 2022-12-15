69c784e2296506eb311a003bff615515e0c4161c
talexandru <talexandru@opusonesolutions.com>
2022-12-15 17:13:26 -0500

    side nav

---
+++ src/app/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatSidenavModule],
+  exports: [CommonModule, MatButtonModule, MatIconModule, MatSidenavModule],
+})
+export class MaterialModule {}
