04d6f1bfaec21552361f391a9309ccb7cc066746
talexandru <talexandru@opusonesolutions.com>
2022-12-15 23:13:24 -0500

    tabs

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -4,16 +4,10 @@ import { CommonModule } from '@angular/common';
 import { OrdersRoutingModule } from './orders-routing.module';
 import { OrdersComponent } from './orders.component';
 import { OrderListComponent } from './order-list/order-list.component';
-
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
-  declarations: [
-    OrdersComponent,
-    OrderListComponent
-  ],
-  imports: [
-    CommonModule,
-    OrdersRoutingModule
-  ]
+  declarations: [OrdersComponent, OrderListComponent],
+  imports: [CommonModule, OrdersRoutingModule, MatTabsModule],
 })
-export class OrdersModule { }
+export class OrdersModule {}
