d04ff6bb45dadd5df18ee083f0559c977720cfc2
talexandru <talexandru@opusonesolutions.com>
2022-12-16 11:45:05 -0500

    datepicker

--- src/app/orders/orders-routing.module.ts
+++ src/app/orders/orders-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { OrderListComponent } from './order-list/order-list.component';
 
-const routes: Routes = [{ path: '', component: OrderListComponent }];
+import { OrderListComponent } from './order-list/order-list.component';
+import { OrderNewComponent } from './order-new/order-new.component';
+
+const routes: Routes = [
+  { path: '', component: OrderListComponent },
+  {
+    path: 'new',
+    component: OrderNewComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class OrdersRoutingModule { }
+export class OrdersRoutingModule {}
--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -1,14 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { OrdersRoutingModule } from './orders-routing.module';
+
 import { OrdersComponent } from './orders.component';
 import { OrderListComponent } from './order-list/order-list.component';
+import { OrderNewComponent } from './order-new/order-new.component';
 
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+// import { MatMomentDateModule } from '@angular/material-moment-adapter';
 
 @NgModule({
-  declarations: [OrdersComponent, OrderListComponent],
-  imports: [CommonModule, OrdersRoutingModule, MatTabsModule],
+  declarations: [OrdersComponent, OrderListComponent, OrderNewComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    OrdersRoutingModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatIconModule,
+    MatInputModule,
+    // MatMomentDateModule,
+    MatNativeDateModule,
+  ],
 })
 export class OrdersModule {}
