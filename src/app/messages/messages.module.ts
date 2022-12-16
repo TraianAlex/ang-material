3bbf7c5cf9ff57a1795b592ec3bdc0286b080419
talexandru <talexandru@opusonesolutions.com>
2022-12-16 09:48:21 -0500

    stepper

--- src/app/messages/messages.module.ts
+++ src/app/messages/messages.module.ts
@@ -1,19 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { MessagesRoutingModule } from './messages-routing.module';
+
 import { MessagesComponent } from './messages.component';
 import { MessageListComponent } from './message-list/message-list.component';
+import { MessageNewComponent } from './message-new/message-new.component';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatStepperModule } from '@angular/material/stepper';
 
 @NgModule({
-  declarations: [
-    MessagesComponent,
-    MessageListComponent
-  ],
+  declarations: [MessagesComponent, MessageListComponent, MessageNewComponent],
   imports: [
     CommonModule,
-    MessagesRoutingModule
-  ]
+    MessagesRoutingModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatStepperModule,
+  ],
 })
-export class MessagesModule { }
+export class MessagesModule {}
