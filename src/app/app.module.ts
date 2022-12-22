306ecb0bc82b85f2ca89a65eea3d4718d67cc8a4
talexandru <talexandru@opusonesolutions.com>
2022-12-22 15:15:14 -0500

    model, auth service, login, logout, header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,7 +32,7 @@ import { StopTrainingDialogComponent } from './training/stop-training-dialog/sto
     WelcomeComponent,
     StopTrainingDialogComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, FormsModule, MaterialModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, FormsModule, ReactiveFormsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent],
 })
