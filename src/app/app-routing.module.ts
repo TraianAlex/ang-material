bd9b229af00e4ecd97d73d436e225af99c5a7518
talexandru <talexandru@opusonesolutions.com>
2022-12-22 15:55:14 -0500

    route protection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { TrainingComponent } from './training/training.component';
 
+import { AuthGuard } from './auth/auth.guard';
+
 const routes: Routes = [
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   { path: 'welcome', component: WelcomeComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'training', component: TrainingComponent },
+  { path: 'training', component: TrainingComponent, canActivate: [AuthGuard] },
   { path: 'orders', loadChildren: () => import('./orders/orders.module').then((m) => m.OrdersModule) },
   { path: 'customers', loadChildren: () => import('./customers/customers.module').then((m) => m.CustomersModule) },
   { path: 'messages', loadChildren: () => import('./messages/messages.module').then((m) => m.MessagesModule) },
