8fa4ef04b87f25d84667b55f397acba03b9c878b
talexandru <talexandru@opusonesolutions.com>
2022-12-23 14:00:53 -0500

    fix training type, set ex duration

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 
+import { TrainingService } from 'src/app/services/training.service';
+
 import { StopTrainingDialogComponent } from '../stop-training-dialog/stop-training-dialog.component';
 
 @Component({
@@ -13,7 +15,7 @@ export class CurrentTrainingComponent implements OnInit {
   timer!: number | NodeJS.Timer;
   @Output() trainingExit = new EventEmitter();
 
-  constructor(private dialog: MatDialog) {}
+  constructor(private dialog: MatDialog, private trainingService: TrainingService) {}
 
   ngOnInit(): void {
     this.startOrResumeTimer();
@@ -25,12 +27,13 @@ export class CurrentTrainingComponent implements OnInit {
   }
 
   private startOrResumeTimer() {
+    const step = this.trainingService.getRunningExercise().duration / 100 * 1000;
     this.timer = setInterval(() => {
       this.progress = this.progress + 1;
       if (this.progress >= 100) {
         clearInterval(this.timer);
       }
-    }, 1000);
+    }, step);
   }
 
   private openDialog() {
